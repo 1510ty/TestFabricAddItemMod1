8bce7f9904c07dec0a34713addb523e90002f442
1510ty <1510ty2580@gmail.com>
2025-01-14 21:56:49 +0900

    クリエイティブインベントリにグループを追加

--- src/main/java/com/mc1510ty/testfabricadditemmod1/Testfabricadditemmod1.java
+++ src/main/java/com/mc1510ty/testfabricadditemmod1/Testfabricadditemmod1.java
@@ -1,10 +1,13 @@
 package com.mc1510ty.testfabricadditemmod1;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
@@ -19,11 +22,29 @@ public class Testfabricadditemmod1 implements ModInitializer {
 
     }
 
+    public final class testfabricadditemmod1_test_group_1 {
+        public static final ItemGroup TEST_GROUP = FabricItemGroup.builder()
+                .icon(() -> new ItemStack(Testfabricadditemmod1.CUSTOM_ITEM))
+                .displayName(Text.translatable("itemGroup.testfabricadditemmod1.test_group_1"))
+                .entries((context, entries) -> {
+                    entries.add(Testfabricadditemmod1.CUSTOM_ITEM);
+                })
+                .build();
+
+        public static void initialize() {
+            // Since 1.21:
+            Registry.register(Registries.ITEM_GROUP, Identifier.of("testfabricadditemmod1", "test_group_1"), TEST_GROUP);
+
+        }
+    }
+
     @Override
     public void onInitialize() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
-            content.add(Testfabricadditemmod1.CUSTOM_ITEM);
-        });
+        testfabricadditemmod1_test_group_1.initialize();
+    //    ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
+    //        content.add(Testfabricadditemmod1.CUSTOM_ITEM);
+    //    });
     }
+
 }
 
