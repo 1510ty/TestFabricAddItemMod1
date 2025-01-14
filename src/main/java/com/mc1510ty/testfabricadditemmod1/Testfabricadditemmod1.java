1656ff8f700169094f5c8d7769c2af489afae565
1510ty <1510ty2580@gmail.com>
2025-01-14 19:48:03 +0900

    クリエイティブインベントリに仮で追加

--- src/main/java/com/mc1510ty/testfabricadditemmod1/Testfabricadditemmod1.java
+++ src/main/java/com/mc1510ty/testfabricadditemmod1/Testfabricadditemmod1.java
@@ -1,8 +1,8 @@
 package com.mc1510ty.testfabricadditemmod1;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.*;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -11,20 +11,19 @@ import java.util.function.Function;
 
 public class Testfabricadditemmod1 implements ModInitializer {
 
-    private void TutorialItems() {
-    }
-
     public static final Item CUSTOM_ITEM = register("testitem1", Item::new, new Item.Settings());
 
     public static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("testfabricadditemmod1", path));
         return Items.register(registryKey, factory, settings);
-    }
 
-    public static void initialize() {
     }
 
     @Override
     public void onInitialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
+            content.add(Testfabricadditemmod1.CUSTOM_ITEM);
+        });
     }
 }
+
